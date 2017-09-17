cb90a6a63b1c5382a45592d7375c5884534dc227
Kahlin Baughman <kahlin@hawaii.edu>
2017-09-16 17:19:42 -1000

    finish WOD

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+const alphaList = ['a', 'b', 'c'];
+
+const numList = [1, 2, 3];
+
+function zipList(list1, list2){
+  const answer = [];
+  if (list1.length === list2.length) {
+    for (let i = 0; i < list1.length; i++) {
+      answer.pop(list1[i]);
+      answer.pop(list2[i]);
+    }
+  } else {
+    console.log('Please use lists of equal lengths.');
+  }
+  return answer;
+}
+
+function zipListUnder(list1, list2) {
+  return _.zip(list1, list2);
+}
+
+console.log(zipListUnder(alphaList, numList));
+console.log(zipListUnder(alphaList, numList));
